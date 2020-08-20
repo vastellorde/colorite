3f5e35f3bf11100b081faaf75f32b312fe36b719
vastellorde <arman@thefroot.com>
2020-08-20 17:22:00 +0500

    (core) init index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, {FunctionComponent} from "react";
 import "./App.scss";
 import {Toolbar} from "./components/toolbar/toolbar";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {IndexPage} from "./pages/index";
 
 export const App: FunctionComponent = () =>  {
     return (
         <BrowserRouter>
             <main>
                 <Toolbar/>
+                <Switch>
+                    <Route path="">
+                        <IndexPage />
+                    </Route>
+                    <Route path="services"></Route>
+                    <Route path="portfolio"></Route>
+                    <Route path="about_us"></Route>
+                    <Route path="request"></Route>
+                </Switch>
             </main>
         </BrowserRouter>
     );
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,7 @@
+import React, {FunctionComponent} from "react";
+
+export const IndexPage: FunctionComponent = () => {
+    return (
+        <div>index</div>
+    )
+}
