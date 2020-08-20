a4f4dcdb4d24c7a8c2425e631eabe1a8fe713190
vastellorde <arman@thefroot.com>
2020-08-20 17:13:44 +0500

    (core) initial project and initial toolbar component

---
+++ src/components/toolbar/toolbar.tsx
@@ -0,0 +1,35 @@
+import React, {FunctionComponent} from 'react';
+import './toolbar.scss';
+import {Link} from "react-router-dom";
+
+export const Toolbar: FunctionComponent = () => {
+    return (
+        <header className="toolbar">
+            <span className="logo">
+                <span>
+                    Color
+                </span>
+                <span>
+                    ite
+                </span>
+                <span className="blink">]</span>
+            </span>
+            <ul>
+                <li>
+                    <Link to="" className="link">Услуги</Link>
+                </li>
+                <li>
+                    <Link to="" className="link">Портфолио</Link>
+                </li>
+                <li>
+                    <Link to="" className="link">О нас</Link>
+                </li>
+                <li>
+                    <Link to="" className="btn">
+                        Оставить заявку <span className="blink">_</span>
+                    </Link>
+                </li>
+            </ul>
+        </header>
+    );
+}
