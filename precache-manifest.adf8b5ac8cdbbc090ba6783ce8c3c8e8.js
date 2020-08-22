8eb510181145ee650deb7cf9a597ef91b2af3938
vastellorde <arman@thefroot.com>
2020-08-22 13:11:02 +0500

    Updates

---
+++ precache-manifest.adf8b5ac8cdbbc090ba6783ce8c3c8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eece14511bf839a0c00966e2919c3d75",
+    "url": "/colorite/index.html"
+  },
+  {
+    "revision": "82021367543d999cea08",
+    "url": "/colorite/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "fabf56891c86e622429d",
+    "url": "/colorite/static/css/main.43a02a40.chunk.css"
+  },
+  {
+    "revision": "82021367543d999cea08",
+    "url": "/colorite/static/js/2.ad977fd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/colorite/static/js/2.ad977fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabf56891c86e622429d",
+    "url": "/colorite/static/js/main.99264628.chunk.js"
+  },
+  {
+    "revision": "9b3c7329b47669376099",
+    "url": "/colorite/static/js/runtime-main.c9fca776.js"
+  }
+]);
