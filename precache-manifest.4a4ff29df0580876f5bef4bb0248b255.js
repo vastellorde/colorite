145ddaeb59ada8ac776ac92cc8320198bcd7482f
vastellorde <arman@thefroot.com>
2020-08-31 12:48:58 +0500

    Updates

---
+++ precache-manifest.4a4ff29df0580876f5bef4bb0248b255.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20556889533a9740102cb4b986fa20ca",
+    "url": "/colorite/index.html"
+  },
+  {
+    "revision": "01408178944ef385fe20",
+    "url": "/colorite/static/css/2.a1063ac4.chunk.css"
+  },
+  {
+    "revision": "74cf66cf1d72e2acf291",
+    "url": "/colorite/static/css/main.47f3f946.chunk.css"
+  },
+  {
+    "revision": "01408178944ef385fe20",
+    "url": "/colorite/static/js/2.8b343644.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/colorite/static/js/2.8b343644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74cf66cf1d72e2acf291",
+    "url": "/colorite/static/js/main.3d2caac6.chunk.js"
+  },
+  {
+    "revision": "9b3c7329b47669376099",
+    "url": "/colorite/static/js/runtime-main.c9fca776.js"
+  },
+  {
+    "revision": "059f9c7882315a6eaa15afb636e5bc5e",
+    "url": "/colorite/static/media/hero.059f9c78.svg"
+  }
+]);
