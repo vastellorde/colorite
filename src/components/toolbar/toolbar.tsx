b25f9ab6e021cd0e3275078ff095bfe1b89802d5
vastellorde <arman@thefroot.com>
2020-08-31 12:40:33 +0500

    (core) animate burger button

--- src/components/toolbar/toolbar.tsx
+++ src/components/toolbar/toolbar.tsx
@@ -1,8 +1,14 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import "./toolbar.scss";
 import { Link } from "react-router-dom";
 
 export const Toolbar: FunctionComponent = () => {
+  const [active, setActive] = useState(false);
+
+  const onBurgerClick = () => {
+    setActive(!active);
+  };
+
   return (
     <div className="container">
       <header className="toolbar">
@@ -33,23 +39,13 @@ export const Toolbar: FunctionComponent = () => {
             </Link>
           </li>
         </ul>
-        <button className="burger">
-          <svg
-            width="32"
-            height="32"
-            viewBox="0 0 32 32"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M26.7333 18.6667H5.26667C4.56711 18.6667 4 19.2338 4 19.9333V20.0667C4 20.7662 4.56711 21.3333 5.26667 21.3333H26.7333C27.4329 21.3333 28 20.7662 28 20.0667V19.9333C28 19.2338 27.4329 18.6667 26.7333 18.6667Z"
-              fill="black"
-            />
-            <path
-              d="M26.7333 10.6667H5.26667C4.56711 10.6667 4 11.2338 4 11.9333V12.0667C4 12.7662 4.56711 13.3333 5.26667 13.3333H26.7333C27.4329 13.3333 28 12.7662 28 12.0667V11.9333C28 11.2338 27.4329 10.6667 26.7333 10.6667Z"
-              fill="black"
-            />
-          </svg>
+        <button
+          onClick={onBurgerClick}
+          className={active ? "burger open" : "burger"}
+        >
+          <span className="burger__line"></span>
+          <span className="burger__line"></span>
+          <span className="burger__line"></span>
         </button>
       </header>
     </div>
